209e113b5ec3ff8caec86893a0ed27b16c1eb924
zachary0kent <73757337+zachary0kent@users.noreply.github.com>
2023-02-21 13:57:46 -0500

    Added a welcome message (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <welcome-message class="welcome-message" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import WelcomeMessage from "./components/WelcomeMessage.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    WelcomeMessage,
   },
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: ProximaNova, sans-serif;
+}
+
+.welcome-message {
+  margin-top: 5%;
 }
 </style>
---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="message">Welcome to CoursePlan!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+.message {
+  text-align: center;
+}
+</style>
